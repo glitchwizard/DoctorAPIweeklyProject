d6d5c1462d73100d3047fef8daa8143c83a2e41b
glitchwizard <glitchwizard@gmail.com>
2019-01-18 15:01:49 -0800

    Saerch is now returning everything properly.

--- src/doctor.js
+++ src/doctor.js
@@ -1,26 +1,10 @@
 import { API } from './api'
+import $ from 'jquery';
 
 class Doctor {
     constructor() {
     }
-
-    static returnDoctorFromQuery(query) {
-        let promise = API.returnDoctorResponse(query);
-        promise.then(function (response) {
-            this.jsonDocResponse = JSON.parse(response);
-            $('#doctorResult').text('');
-            for (var doctorIndex = 0; doctorIndex < 10; doctorIndex++) {
-                $('#doctorResult').append(`
-          <p class='doctorSearchResult' id='${this.jsonDocResponse.data[doctorIndex].uid}'>
-          <img src=${this.jsonDocResponse.data[doctorIndex].profile.image_url}>
-          ${this.jsonDocResponse.data[doctorIndex].profile.first_name} ${this.jsonDocResponse.data[doctorIndex].profile.last_name} Practice:<a href=""> ${this.jsonDocResponse.data[doctorIndex].practices[0].name}
-          </a>
-          </p>`);
-            }
-        });
-    }
+   
 }
-
-Doctor.jsonDocResponse = "";
 
 export { Doctor };
--- src/main.js
+++ src/main.js
@@ -6,7 +6,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 function openSymptomSearch() {
+
   document.getElementById("myDropdown").classList.toggle("show");
+
+  getSymptomAPIresult();
 }
 
 function filterFunction() {
@@ -26,29 +29,44 @@ function filterFunction() {
   }
 }
 
-$(document).ready(function () {
+function returnDoctorFromQuery(query) {
+  let jsonDocResponse;
+  let promise = API.returnDoctorResponse(query);
+  promise.then(function (response) {
+    jsonDocResponse = JSON.parse(response);
+    $('#doctorResult').text('');
+    for (var doctorIndex = 0; doctorIndex <= jsonDocResponse.data.length-1; doctorIndex++) {
+      $('#doctorResult').append(`<div class=docSearchResult">
+          <p class='doctorSearchResult' id='${jsonDocResponse.data[doctorIndex].uid}'>
+          <img src=${jsonDocResponse.data[doctorIndex].profile.image_url}>
+          ${jsonDocResponse.data[doctorIndex].profile.first_name} ${jsonDocResponse.data[doctorIndex].profile.last_name} Practice: ${jsonDocResponse.data[doctorIndex].practices[0].name}</p>`);
+      if (jsonDocResponse.data[doctorIndex].practices[0].website != undefined) {
+        $('#doctorResult').append(`<p> Website: <a href="${jsonDocResponse.data[doctorIndex].practices[0].website}">${jsonDocResponse.data[doctorIndex].practices[0].website}</a></p>`)
+      }
+      $('#doctorResult').append(`</div>`)
+    }
+  });
+}
+
+function getSymptomAPIresult() {
   let jsonResponse;
+  let promise = API.returnSymptomResponse()
+  promise.then(function (response) {
+    jsonResponse = JSON.parse(response);
+    $('#searchList').text('');
+    let dataNum = jsonResponse.data.length;
+    for (var symptomIndex = 0; symptomIndex < 10; symptomIndex++) {
+      $('#searchList').append(`<p class='symptomSearchResult' id='${jsonResponse.data[symptomIndex].name}'>${jsonResponse.data[symptomIndex].name}</p>`);
+    }
+  });
+}
+
+$(document).ready(function () {
+
 
   $('#triggerAPI').click(function (event) {
-    let promise = API.returnSymptomResponse()
-    promise.then(function (response) {
-      jsonResponse = JSON.parse(response);
-      let dataNum = jsonResponse.data.length;
-      for (var symptomIndex = 0; symptomIndex < 10; symptomIndex++) {
-        $('#searchList').append(`<p class='symptomSearchResult' id='${jsonResponse.data[symptomIndex].name}'>${jsonResponse.data[symptomIndex].name}</p>`);
-      }
-    });
-
+    getSymptomAPIresult();
   });
-
-  //let promise = API.returnSymptomResponse()
-  //promise.then(function (response) {
-  //  jsonResponse = JSON.parse(response);
-  //  let dataNum = jsonResponse.data.length;
-  //  for (var symptomIndex = 0; symptomIndex < 10; symptomIndex++) {
-  //    $('#searchList').append(`<p class='symptomSearchResult' id='${jsonResponse.data[symptomIndex].uid}'>${jsonResponse.data[symptomIndex].name}</p>`);
-  //  }
-  //});
 
   $('#myInput').keyup(function () {
     filterFunction();
@@ -69,15 +87,13 @@ $(document).ready(function () {
 
   $('#doctorSearchButton').click(function () {
     let queryInput = $('#mySymptomInput').val();
-    Doctor.returnDoctorFromQuery(queryInput);
+    returnDoctorFromQuery(queryInput);
   })
 
   $('#mySymptomInput').on('keypress', function (e) {
     if (e.which == 13) {
       let queryInput = $('#mySymptomInput').val();
-      Doctor.returnDoctorFromQuery(queryInput);
+      returnDoctorFromQuery(queryInput);
     }
   });
 });
-
-
