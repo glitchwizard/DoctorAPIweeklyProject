ccf5a56577a41d55701a6d257da0393f2324d803
glitchwizard <glitchwizard@gmail.com>
2019-01-18 15:49:28 -0800

    Cleaned up page and started making search results match what the requirements are.

--- src/main.js
+++ src/main.js
@@ -5,95 +5,59 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-function openSymptomSearch() {
-
-  document.getElementById("myDropdown").classList.toggle("show");
-
-  getSymptomAPIresult();
-}
-
-function filterFunction() {
-  console.log("filter working");
-  var input, filter, a, i, div, txtValue;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
-  a = div.getElementsByTagName("p");
-  for (i = 0; i < a.length; i++) {
-    txtValue = a[i].textContent || a[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      a[i].style.display = "";
-    } else {
-      a[i].style.display = "none";
-    }
-  }
-}
-
-function returnDoctorFromQuery(query) {
+function returnDoctorFromSymptomQuery(query) {
   let jsonDocResponse;
   let promise = API.returnDoctorResponse(query);
   promise.then(function (response) {
     jsonDocResponse = JSON.parse(response);
     $('#doctorResult').text('');
-    for (var doctorIndex = 0; doctorIndex <= jsonDocResponse.data.length-1; doctorIndex++) {
-      $('#doctorResult').append(`<div class=docSearchResult">
-          <p class='doctorSearchResult' id='${jsonDocResponse.data[doctorIndex].uid}'>
-          <img src=${jsonDocResponse.data[doctorIndex].profile.image_url}>
-          ${jsonDocResponse.data[doctorIndex].profile.first_name} ${jsonDocResponse.data[doctorIndex].profile.last_name} Practice: ${jsonDocResponse.data[doctorIndex].practices[0].name}</p>`);
-      if (jsonDocResponse.data[doctorIndex].practices[0].website != undefined) {
-        $('#doctorResult').append(`<p> Website: <a href="${jsonDocResponse.data[doctorIndex].practices[0].website}">${jsonDocResponse.data[doctorIndex].practices[0].website}</a></p>`)
-      }
-      $('#doctorResult').append(`</div>`)
-    }
-  });
-}
 
-function getSymptomAPIresult() {
-  let jsonResponse;
-  let promise = API.returnSymptomResponse()
-  promise.then(function (response) {
-    jsonResponse = JSON.parse(response);
-    $('#searchList').text('');
-    let dataNum = jsonResponse.data.length;
-    for (var symptomIndex = 0; symptomIndex < 10; symptomIndex++) {
-      $('#searchList').append(`<p class='symptomSearchResult' id='${jsonResponse.data[symptomIndex].name}'>${jsonResponse.data[symptomIndex].name}</p>`);
+    for (var doctorIndex = 0; doctorIndex <= jsonDocResponse.data.length - 1; doctorIndex++) {
+      let currentDoctor = jsonDocResponse.data[doctorIndex];
+      $('#doctorResult').append(`<hr /><div class=docSearchResult">`);
+
+      $('#doctorResult').append(`<p class='doctorSearchResult' id='${currentDoctor.uid}'><h3>${currentDoctor.profile.first_name} ${currentDoctor.profile.last_name}</h3></p><p><strong>Practice:</strong> ${currentDoctor.practices[0].name}</p><p><strong>Address:</strong> ${currentDoctor.practices[0].visit_address.street} ${currentDoctor.practices[0].visit_address.street2}</p><p>${currentDoctor.practices[0].visit_address.city} ${currentDoctor.practices[0].visit_address.state},${currentDoctor.practices[0].visit_address.zip}<p>`);
+
+      if (currentDoctor.practices[0].website != undefined) {
+        $('#doctorResult').append(`<p> Website: <a href="${currentDoctor.practices[0].website}">${currentDoctor.practices[0].website}</a></p>`)
+      }
+
+      for (var currentPhoneIndex = 0; currentPhoneIndex < currentDoctor.practices[0].phones.length; currentPhoneIndex++) {
+        if (currentDoctor.practices[0].phones[currentPhoneIndex].type == 'landline') {
+          $('#doctorResult').append(`<p><strong>Phone Number:</strong>${currentDoctor.practices[0].phones[currentPhoneIndex].number}</p>`)
+        }
+      }
+
+      if (currentDoctor.practices[0].accepts_new_patients == true) {
+        $('#doctorResult').append(`<p><strong>Accepting new patients: </strong>Yes</p>`);
+      } else if (currentDoctor.practices[0].accepts_new_patients == false) {
+        $('#doctorResult').append(`<p><strong>Accepting new patients: </strong>No</p>`);
+      } else {
+        $('#doctorResult').append(`<p><strong>Accepting new patients: </strong>Unknown</p>`)
+      }
+
+
+      $('#doctorResult').append(`</div>`)
     }
   });
 }
 
 $(document).ready(function () {
 
-
-  $('#triggerAPI').click(function (event) {
-    getSymptomAPIresult();
-  });
-
-  $('#myInput').keyup(function () {
-    filterFunction();
-  });
-
-  $("#searchList").click(function () {
-    console.log($('.symptomSearchResult').attr('id'));
-  });
-
-  $('#symptomSearchButton').click(function () {
-    openSymptomSearch();
-  });
-
   $('#doctorSearchButton').click(function () {
     let queryInput = $('#mySymptomInput').val();
-    Doctor.returnDoctorFromQuery(queryInput);
+    Doctor.returnDoctorFromSymptomQuery(queryInput);
   })
 
   $('#doctorSearchButton').click(function () {
     let queryInput = $('#mySymptomInput').val();
-    returnDoctorFromQuery(queryInput);
+    returnDoctorFromSymptomQuery(queryInput);
   })
 
   $('#mySymptomInput').on('keypress', function (e) {
     if (e.which == 13) {
       let queryInput = $('#mySymptomInput').val();
-      returnDoctorFromQuery(queryInput);
+      returnDoctorFromSymptomQuery(queryInput);
     }
   });
 });
