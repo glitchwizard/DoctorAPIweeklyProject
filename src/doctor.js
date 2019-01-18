24d9f4a93bec6d79ac5679d8e69f785ab229d92e
glitchwizard <glitchwizard@gmail.com>
2019-01-18 11:29:13 -0800

    Got promises working and returning data

--- src/doctor.js
+++ src/doctor.js
@@ -1,8 +1,8 @@
 class Doctor {
     constructor() {
 
-  }
-    
+    }
+
 }
 
 export { Doctor };
--- src/main.js
+++ src/main.js
@@ -1,68 +1,55 @@
 import { Doctor } from './doctor';
+import { API } from './api'
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-function symptomSearch() {
-    // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('symptomInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("symptomList");
-    li = ul.getElementsByTagName('li');
-
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
+function openSymptomSearch() {
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
-function symptomSearch() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-function symptomFilterFunction() {
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    div = document.getElementById("myDropdown");
-    a = div.getElementsByTagName("a");
-    for (i = 0; i < a.length; i++) {
-        txtValue = a[i].textContent || a[i].innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            a[i].style.display = "";
-        } else {
-            a[i].style.display = "none";
-        }
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("p");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
     }
+  }
 }
 
 $(document).ready(function () {
 
-    $('#triggerAPI').click(function (event) {
 
-        let request = new XMLHttpRequest();
-        const url = `https://api.betterdoctor.com/2016-03-01/conditions?user_key=${process.env.exports.apiKey}`;
 
-        request.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status === 200) {
-                const response = JSON.parse(this.responseText);
-                getElements(response);
-            }
-        }
-        request.open("GET", url, true);
-        request.send();
-
-        const getElements = function (response) {
-            console.log(response);
-        }
-        
+  $('#triggerAPI').click(function (event) {
+    let promise = API.returnSymptomResponse()
+      promise.then(function (response) {
+      console.log('response data');
+      console.log(JSON.parse(response).data);
     });
+
+
+
+    //const getElements = function (response) {
+    //  console.log(response)
+    //}
+
+    //const printThing = function(response) {
+    //  let dataNum = response.data.length;
+    //  for (var symptomIndex = 0; symptomIndex < 10; symptomIndex++) {
+    //      console.log(response.data[symptomIndex].name);
+    //    $('input#myInput').append(`<p>${response.data[symptomIndex].name}</p>`);
+    //    $('#testList').append(`<li>${response.data[symptomIndex].name}</li>`);
+    //  }
+    //}
+
+  });
 });
